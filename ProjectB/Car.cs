f1736961d1952bbfad93ede007399bf8c4a3bd69
Jeremey <1028294@hr.nl>
2022-02-21 19:35:05 +0100

    Car commit

---
+++ ProjectB/Car.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectB
+{
+    internal class Car
+    {
+        public void GiveGass()
+        {
+            Console.WriteLine("Vroom");
+        }
+    }
+}
